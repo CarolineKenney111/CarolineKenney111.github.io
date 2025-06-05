821638b4c5db6b612035bdd4fa93df80bec2034e
CarolineKenney111 <cake1896@colorado.edu>
2025-06-05 14:54:54 -0400

    push

--- InternFacebook/FB.js
+++ InternFacebook/FB.js
@@ -1,15 +1,14 @@
 function filterInterns() {
-    const input = document.getElementById('searchInput').value.toLowerCase();
-    const cards = document.querySelectorAll('.card');
-  
-    cards.forEach(card => {
-      const name = card.querySelector('h3').textContent.toLowerCase();
-      const school = card.querySelector('p').textContent.toLowerCase();
-      if (name.includes(input) || school.includes(input)) {
-        card.style.display = '';
-      } else {
-        card.style.display = 'none';
-      }
-    });
-  }
-  
+  const input = document.getElementById('searchInput').value.toLowerCase();
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    const name = card.querySelector('h3').textContent.toLowerCase();
+    const school = card.querySelector('p').textContent.toLowerCase();
+    if (name.includes(input) || school.includes(input)) {
+      card.style.display = '';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
